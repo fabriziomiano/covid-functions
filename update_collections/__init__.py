bcef594bf8d0bbc019ad199a678a6b88a25515f1
Fabrizio Miano <fabriziomiano@gmail.com>
2020-03-12 14:23:11 +0100

    fix "data" key Load/Transormation

--- update_collections/__init__.py
+++ update_collections/__init__.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+import datetime as dt
 import logging
 import requests
 import azure.functions as func
@@ -9,6 +9,10 @@ NATIONAL_DATA_URL = os.environ["NATIONAL_DATA_URL"]
 REGIONAL_DATA_URL = os.environ["REGIONAL_DATA_URL"]
 PROVINCIAL_DATA_URL = os.environ["PROVINCIAL_DATA_URL"]
 DB_NAME = os.environ["DB_NAME"]
+try:
+    PCM_DATE_FMT = os.environ["PCM_DATE_FMT"]
+except KeyError:
+    PCM_DATE_FMT = "%Y-%m-%d %H:%M:%S"
 
 national_data = requests.get(NATIONAL_DATA_URL).json()
 regional_data = requests.get(REGIONAL_DATA_URL).json()
@@ -16,8 +20,8 @@ provincial_data = requests.get(PROVINCIAL_DATA_URL).json()
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+    utc_timestamp = dt.datetime.utcnow().replace(
+        tzinfo=dt.timezone.utc).isoformat()
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
@@ -28,6 +32,8 @@ def main(mytimer: func.TimerRequest) -> None:
     provincial_collection = client[DB_NAME].provincial
     logging.info("Updating national collection")
     for d in national_data:
+        date_dt = dt.datetime.strptime(d["data"], PCM_DATE_FMT)
+        d["data"] = date_dt
         national_collection.update_one(
             {"data": d["data"]},
             {"$set": d},
@@ -35,6 +41,8 @@ def main(mytimer: func.TimerRequest) -> None:
         )
     logging.info("Updating regional collection")
     for d in regional_data:
+        date_dt = dt.datetime.strptime(d["data"], PCM_DATE_FMT)
+        d["data"] = date_dt        
         regional_collection.update_one(
             {"data": d["data"]},
             {"$set": d},
@@ -42,6 +50,8 @@ def main(mytimer: func.TimerRequest) -> None:
         )
     logging.info("Updating provincial collection")
     for d in provincial_data:
+        date_dt = dt.datetime.strptime(d["data"], PCM_DATE_FMT)
+        d["data"] = date_dt        
         provincial_collection.update_one(
             {"data": d["data"]},
             {"$set": d},
